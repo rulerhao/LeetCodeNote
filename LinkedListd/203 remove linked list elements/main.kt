36fd858af7abfd6193b191e56df063fccbec6ffd
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-08-03 23:32:19 +0800

    Added code for 203.

--- LinkedListd/203 remove linked list elements/main.kt
+++ LinkedListd/203 remove linked list elements/main.kt
@@ -1,1 +1,19 @@
-
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        // Null Check
+        if (head == null)
+            return head;
+        ListNode dummyNode = new ListNode(-1, head);
+        ListNode pre = dummyNode;
+        ListNode cur = dummyNode.next;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummyNode.next;
+    }
+}
