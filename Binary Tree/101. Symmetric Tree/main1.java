8d15bc0dcf0070d8102b0d1fdbba06299833b512
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-08-15 17:57:23 +0800

    Added solution 1 for problem 101.

---
+++ Binary Tree/101. Symmetric Tree/main1.java
@@ -0,0 +1,22 @@
+    public boolean isSymmetric1(TreeNode root) {
+        return compare(root.left, root.right);
+    }
+
+    private boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        boolean compareOutside = compare(left.left, right.right);
+        boolean compareInside = compare(left.right, right.left);
+        return compareOutside && compareInside;
+    }
