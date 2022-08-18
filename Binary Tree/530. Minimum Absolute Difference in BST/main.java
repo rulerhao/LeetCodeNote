548b4ba37bbe8c828ccc57e76a3baec25cb32631
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-08-18 16:45:44 +0800

    Added solution for problem 530.

---
+++ Binary Tree/530. Minimum Absolute Difference in BST/main.java
@@ -0,0 +1,23 @@
+class Solution {
+    TreeNode pre;
+    int result = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+       if(root == null) return 0;
+        
+       traversal(root);
+        
+       return result;
+    }
+    public void traversal(TreeNode root){
+        if(root == null) return;
+        //左
+        traversal(root.left);
+        //中
+        if (pre != null) {
+            result = Math.min(result, root.val - pre.val);
+        }
+        pre = root;
+        //右
+        traversal(root.right);
+    }
+}
