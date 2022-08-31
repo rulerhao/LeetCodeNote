e7cb7a4661cdb1889b5d47137e700a4c1aaf4e69
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-09-01 01:24:23 +0800

    Added solution for problem 77.

---
+++ BackTracking/77. Combinations/main.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    
+    public List<List<Integer>> combine(int n, int k) {
+        combineHelper(n, k, 1);
+        return result;
+    }
+    
+    private void combineHelper(int n, int k, int startIndex) {
+        if (path.size() == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        int max = n - (k - path.size()) + 1;
+        for (int i = startIndex; i <= max; i++) {
+            path.add(i);
+            combineHelper(n, k, i + 1);
+            path.removeLast();
+        }
+    }
+}
