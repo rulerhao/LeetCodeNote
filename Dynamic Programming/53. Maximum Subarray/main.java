22669aa7136114bd197a10295534619a9e047665
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-09-24 11:46:48 +0800

    Added solution for problem 53.

---
+++ Dynamic Programming/53. Maximum Subarray/main.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int maxSubArray(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int res = nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
