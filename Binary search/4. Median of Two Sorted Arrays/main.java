251954ce810fa735936fd71fcc0a9c2104b75816
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-10-19 16:25:49 +0800

    Added solution for problem 4.

---
+++ Binary search/4. Median of Two Sorted Arrays/main.java
@@ -0,0 +1,49 @@
+class Solution {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int total = m + n;
+        int half = (total + 1) / 2;
+
+        int left = 0;
+        int right = m;
+
+        double result = 0.0;
+
+        while (left <= right) {
+            int i = (left + right) / 2;
+            int j = half - i;
+
+            // get the four points around possible median
+            int left1 = (i > 0) ? nums1[i - 1] : Integer.MIN_VALUE;
+            int right1 = (i < m) ? nums1[i] : Integer.MAX_VALUE;
+            int left2 = (j > 0) ? nums2[j - 1] : Integer.MIN_VALUE;
+            int right2 = (j < n) ? nums2[j] : Integer.MAX_VALUE;
+
+            // partition is correct
+            if (left1 <= right2 && left2 <= right1) {
+                // even
+                if (total % 2 == 0) {
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+                // odd
+                } else {
+                    return Math.max(left1, left2);
+                }
+            }
+            // partition is wrong (update left/right pointers)
+            if (left1 > right2) {
+                right = i - 1;
+            } else {
+                left = i + 1;
+            }
+        }
+
+        return result;
+    }
+}
