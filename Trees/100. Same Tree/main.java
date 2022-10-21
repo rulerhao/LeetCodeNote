e7ead3f37cdefd06950fb6bab0d35609a4c8dc8f
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-10-21 14:02:41 +0800

    Added solution for problem 100.

---
+++ Trees/100. Same Tree/main.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return checkSame(p, q);
+    }
+    
+    private boolean checkSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q == null) {
+            return false;
+        } else if (p == null && q != null) {
+            return false;
+        }
+        
+        boolean left = checkSame(p.left, q.left);
+        boolean right = checkSame(p.right, q.right);
+        
+        return p.val == q.val && left && right;
+    }
+}
