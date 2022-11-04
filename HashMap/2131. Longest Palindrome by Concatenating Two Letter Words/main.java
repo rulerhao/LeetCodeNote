80faebd26dd5ab162883d816c85cb83beaece3d4
Jake Tsao <32173057+rulerhao@users.noreply.github.com>
2022-11-04 17:29:35 +0800

    Edit solution for problem 2131.

--- HashMap/2131. Longest Palindrome by Concatenating Two Letter Words/main.java
+++ HashMap/2131. Longest Palindrome by Concatenating Two Letter Words/main.java
@@ -1,39 +1,36 @@
 class Solution {
     public int longestPalindrome(String[] words) {
+        int[] doubleArr = new int[26];
+        HashMap<String, Integer> singleMap = new HashMap<>();
         
-        int[][] dp = new int[26][26];
-        int middleLength = 0;
-
-        for (String word : words) {
-            dp[word.charAt(0) - 'a'][word.charAt(1) - 'a']++;
-        }
-
-        int startLength = 0;
-
-        for (int row = 0; row < 26; row++) {
-            for (int column = 0; column < 26; column++) {
-
-                if (row == column) { //aa
-
-                    if (dp[row][column] % 2 == 0) {
-                        startLength += (dp[row][column] * 2);
-
-                    } else {
-                        middleLength = 2;
-                        startLength += 2 * (dp[row][column] - 1);
-                    }
-
-                } else { //ab
-
-                    if (dp[row][column] > 0 && dp[column][row] > 0) {
-                        startLength += 4 * Math.min(dp[row][column], dp[column][row]);
-                        dp[column][row] = 0;
-                        dp[row][column] = 0;
-                    }
+        int res = 0;
+        for (String str: words) {
+            if (str.charAt(0) == str.charAt(1)) {
+                doubleArr[str.charAt(0) - 'a']++;
+            } else {
+                String reverseStr = "" + str.charAt(1) + str.charAt(0);
+                if (singleMap.getOrDefault(reverseStr, 0) == 0) {
+                    singleMap.put(str, singleMap.getOrDefault(str, 0) + 1);
+                } else {
+                    singleMap.put(reverseStr, singleMap.get(reverseStr) - 1);
+                    res += 4;
                 }
             }
         }
 
-        return startLength + middleLength;
+        boolean usedOdd = false;
+        for (int n: doubleArr) {
+            if (n % 2 == 0) {
+                res += n * 2;
+            } else {
+                res += (n - 1) * 2;
+                if (!usedOdd) {
+                    res += 2;
+                    usedOdd = true;
+                }
+            }
+        }
+
+        return res;
     }
 }
